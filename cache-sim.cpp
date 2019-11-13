f933351625b9b1ff75949d80717273b7be7dab0d
David Walters <dwalter2@binghamton.edu>
2019-11-12 20:07:07 -0500

    working direct map

--- cache-sim.cpp
+++ cache-sim.cpp
@@ -27,17 +27,19 @@ unsigned int directMap(int cachesize, vector<input> input){
 		cache.push_back(c);
 	}
 	for(int i = 0 ; i < input.size() ; i++){
-		int index = input[i].address % cachelines;
+		/*int index = input[i].address % cachelines;
 		unsigned int mask = pow(2,32)-1;
 		unsigned int shiftamount = log2(cachelines);
 		mask = mask << shiftamount;
-		unsigned int indextag = mask & input[i].address;
-		if(cache[index].validbit==1&&cache[index].tag==indextag){
+		unsigned int indextag = mask & input[i].address;*/
+		unsigned long long currindex = (input[i].address >> 5) % (int)pow(2,log2(cachelines));
+		int currtag = input[i].address / pow(2,log2(cachelines));
+		if(cache[currindex].validbit==1&&cache[currindex].tag==currtag){
 			cachehits++;
 		}
 		else{
-			cache[index].tag = indextag;
-			cache[index].validbit = 1;
+			cache[currindex].tag = currtag;
+			cache[currindex].validbit = 1;
 		}
 	}
 	return cachehits;
@@ -56,5 +58,5 @@ int main(int argc, char** argv){
   	}
 	cout << directMap(1024,inputs) << endl;
 	infile.close();
-	
+
 }
